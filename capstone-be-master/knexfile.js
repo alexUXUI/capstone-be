53fc590f673022cfb98dbff6ebe7d14d1cdd0b13
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-19 21:39:15 -0600

    get all users

--- capstone-be-master/knexfile.js
+++ capstone-be-master/knexfile.js
@@ -2,8 +2,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    // connection: 'postgres://fjjabddtxkwgnk:J-iF51qSciarNHPDNttQ7jJ0eU@ec2-54-83-22-48.compute-1.amazonaws.com:5432/dd7mgiunmq9mg0?ssl=true'
-    connection: "postgres://localhost/rebelmarkets"
+    connection: "postgres://localhost/rebelz"
   //   connection: "postgres://ueuououreqloaw:wi3x9ngmeYTAkkuB864Ykyh675@ec2-54-225-151-64.compute-1.amazonaws.com:5432/db9qauvj49sese?ssl=true"
   }
 };
