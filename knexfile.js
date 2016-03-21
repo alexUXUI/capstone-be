7b23476e77b20dd1386f075316ee8b848e840e95
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-21 15:00:11 -0600

    killing nodemonkey

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
+require('dotenv').load();
+
 // Update with your config settings.
 module.exports = {
   development: {
     client: 'pg',
-    // connection: 'postgres://fjjabddtxkwgnk:J-iF51qSciarNHPDNttQ7jJ0eU@ec2-54-83-22-48.compute-1.amazonaws.com:5432/dd7mgiunmq9mg0?ssl=true'
-    connection: "postgres://localhost/rebelmarkets"
-  //   connection: "postgres://ueuououreqloaw:wi3x9ngmeYTAkkuB864Ykyh675@ec2-54-225-151-64.compute-1.amazonaws.com:5432/db9qauvj49sese?ssl=true"
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 };
